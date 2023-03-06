7302e225a51715b663c48d572537e82e6e1e1339
Mohamed Ateya Abdel Jaleel <36847181+GalilioOo@users.noreply.github.com>
2023-03-06 19:42:39 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cat source.text | while read line 
+do
+  # separate line with white space
+  arr=(${line// / })
+
+  # check each line consist from 3 parts name ID Email
+  # get matching email using regex
+
+  if [ ${#arr[@]} -eq '3' ] && [[ "${arr[1]} " =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4} ]]
+  then
+    if [ $((arr[2]%2)) -eq 0 ] 
+    then
+      echo " The ${arr[2]} of ${arr[1]} is EVEN"
+    else
+      echo " The ${arr[2]} of ${arr[1]} is ODD"
+    fi
+  fi
+done
